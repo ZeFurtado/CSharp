500feac65365fffb9b3bca015c92f823154199e2
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-05 00:32:01 -0300

    PassagemParametroOut

---
+++ Estruturas e Passagem de valores/PassagemParametroOut/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+        static void Main(String[] args)
+        {
+            double valor = 25;
+
+            Console.WriteLine($"Main > var valor: {valor}");
+
+            AlterarValor(out valor);
+            int idade;
+            RetornarNomeIdadePessoa(new DateTime(10,10,2000), out idade);
+
+            Console.WriteLine($"Main > var valor {valor}");
+
+            Console.ReadKey();
+        }
+
+        static void AlterarValor(out double valor)
+        {
+            valor = 10;
+        }
+
+        static string RetornarNomeIdadePessoa(DateTime dataNascimento, out int idade)
+        {
+            idade = 15;   
+            return "João Péricles";
+        }
+}
