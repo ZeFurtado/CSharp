5277f9ab115b366d51ca5d28540b19fd5bdb0623
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-05 00:19:54 -0300

    PassagemParametroReferência

---
+++ Estruturas e Passagem de valores/PassagemParametroReferencia/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+        static void Main(String[] args)
+        {
+            double valor = 25;
+
+            Console.WriteLine($"Main > var valor: {valor}");
+
+            AlterarValor(ref valor);
+
+            Console.WriteLine($"Main > var valor {valor}");
+
+            Console.ReadKey();
+        }
+
+        static void AlterarValor(ref double valor)
+        {
+            Console.WriteLine($"AlterarValor > {valor}");
+
+            valor += 10;
+
+            Console.WriteLine($"AlterarValor > {valor}");
+        }
+}
