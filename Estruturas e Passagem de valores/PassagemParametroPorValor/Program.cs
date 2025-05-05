d63ea17143dbfd971af720b07e24872de607207e
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-04 23:56:25 -0300

    ParametroPorValor

--- Estruturas e Passagem de valores/PassagemParametroPorValor/Program.cs
+++ Estruturas e Passagem de valores/PassagemParametroPorValor/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PassagemParametroPorValor
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            double valor = 25;
+
+            Console.WriteLine($"Main > var valor: {valor}");
+
+            AlterarValor(valor);
+
+            Console.WriteLine($"Main > var valor {valor}");
+
+            Console.ReadKey();
+        }
+
+        static void AlterarValor(double valor)
+        {
+            Console.WriteLine($"AlterarValor > {valor}");
+
+            valor += 10;
+
+            Console.WriteLine($"AlterarValor > {valor}");
+        }
+    }
+}
