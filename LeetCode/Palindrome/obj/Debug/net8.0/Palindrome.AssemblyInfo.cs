d9dec0aa870be6f94655313ef488f171bade28c4
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-15 20:49:38 -0300

    Mudanças na pasta obj

--- LeetCode/Palindrome/obj/Debug/net8.0/Palindrome.AssemblyInfo.cs
+++ LeetCode/Palindrome/obj/Debug/net8.0/Palindrome.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Palindrome")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040b482038c5c546f757584be85d223ab6bed014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56599cbfd745f0c289ed79cf534e0332142b5fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Palindrome")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Palindrome")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
