e4164b8906eaf32ad199b2c89147bc9b39ced067
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-21 00:00:39 -0300

    Adicioanado pacote EntityFrameworkCore.Tools

--- RazorSql/obj/Debug/net8.0/RazorSql.AssemblyInfo.cs
+++ RazorSql/obj/Debug/net8.0/RazorSql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorSql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254219ffb47bc6cdacb08e2498e1b37ad6b3c5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ee4335e0734388f7808dbd9efeeef37e7f19e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorSql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorSql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
