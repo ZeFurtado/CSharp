7acfd80c035c9b987430552a62645c8f3f8c33db
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-25 13:43:52 -0300

    PropriedadeAbstrata

---
+++ Classes internas, abstratas, interface e enumeradores/PropriedadeAbstrata/Carro.cs
@@ -0,0 +1,7 @@
+namespace PropriedadeAbstrata;
+
+public class Carro : Veiculo
+{
+    public override string Marca { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public override string Modelo { get => throw new NotImplementedException(); }
+}
---
+++ Classes internas, abstratas, interface e enumeradores/PropriedadeAbstrata/Veiculo.cs
@@ -0,0 +1,7 @@
+namespace PropriedadeAbstrata;
+
+public abstract class Veiculo
+{
+    abstract public string Marca { get; set; }
+    abstract public string Modelo { get; }
+}
