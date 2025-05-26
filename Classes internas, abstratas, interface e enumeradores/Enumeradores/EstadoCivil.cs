5abb79d1da570ff74732eb8586d02846cb968b5e
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-25 22:43:08 -0300

    Enumeradores

---
+++ Classes internas, abstratas, interface e enumeradores/Enumeradores/EstadoCivil.cs
@@ -0,0 +1,6 @@
+namespace Enumeradores;
+
+enum EstadoCivil
+{ 
+    Solteiro, Casado, Viuvo, Divorciado
+}
---
+++ Classes internas, abstratas, interface e enumeradores/Enumeradores/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Enumeradores;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("Estado Civil: " + EstadoCivil.Solteiro);
+        Console.WriteLine("Estado Civil *Númerico: " +(int)EstadoCivil.Solteiro);
+    }
+}
