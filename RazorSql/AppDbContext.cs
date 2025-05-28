8341a8c31b854006dae28aeff67151c1e0791898
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-27 23:19:06 -0300

    Inicio da conexão com banco de dados inciada. Obs.. Falta inciar as migrações

--- RazorSql/AppDbContext.cs
+++ RazorSql/AppDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace RazorSql;
 
-public class AppDbContext :DbContext
+public class AppDbContext : DbContext
 {
-
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Plantas> Plantas { get; set; }
 }
--- RazorSql/Models/Plantas.cs
+++ RazorSql/Models/Plantas.cs
@@ -1,6 +1,9 @@
-﻿namespace RazorSql;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace RazorSql;
+
+[Table("plantas")]
 public class Plantas
 {
-
+    
 }
