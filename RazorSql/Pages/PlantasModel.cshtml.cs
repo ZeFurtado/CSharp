62d1457d5f51d9e016f18ed1c313e91772bdd96f
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-27 23:11:46 -0300

    [Página teste]Criado modelo para página das plantas

---
+++ RazorSql/Pages/PlantasModel.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorSql.Pages;
+
+public class PlantasModel : PageModel
+{ 
+    private readonly AppDbContext _context;
+
+    public PlantasModel(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public required IList<Plantas> Plantas { get; set; }
+
+    
+    public async Task OnGetAsync()
+    {
+        //Plantas = await _context.Plantas.ToListAsync();
+    }
+}
