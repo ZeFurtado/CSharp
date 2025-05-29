a2fc9ed00d4af60ba6b5df2af9ad548bfa03fd79
ZeFurtado <lukhasfurtado9@gmail.com>
2025-05-29 00:23:15 -0300

    Propriedades da classe modelo Plantas.cs

--- RazorSql/Models/Plantas.cs
+++ RazorSql/Models/Plantas.cs
@@ -5,5 +5,7 @@ namespace RazorSql;
 [Table("plantas")]
 public class Plantas
 {
-    
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
 }
