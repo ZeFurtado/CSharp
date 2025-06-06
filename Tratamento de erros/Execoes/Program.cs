bcde3babb11487ae134e1ff7a73534c193525586
ZeFurtado <lukhasfurtado9@gmail.com>
2025-06-05 22:48:20 -0300

    Tratamento de erros

---
+++ Tratamento de erros/Execoes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Execoes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite o número 1: ");
+        string digitado = Console.ReadLine();
+
+        if (digitado == "1")
+        {
+            Console.WriteLine("Correto");
+        }
+        else
+        {
+            throw new Exception("O valor digitado está incorreto");   
+        }
+    }
+}
