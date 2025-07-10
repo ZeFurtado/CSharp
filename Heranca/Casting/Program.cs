b75989683bbfff545f5ea4c6aaa4c224625989c6
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-10 20:25:46 -0300

    Casting

---
+++ Heranca/Casting/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Casting;
+
+
+public class Program
+{
+    static void Main()
+    {
+        double preco = 3125.04;
+        int a = (int)preco;
+
+        a = Convert.ToInt32(preco);
+    }
+}
