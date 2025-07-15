171297699d2c8c7891168fb40339873c18d77286
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-14 22:19:50 -0300

    Carta

---
+++ OpenTK/Detetive/Carta.cs
@@ -0,0 +1,15 @@
+﻿using System.Dynamic;
+
+namespace Detetive;
+
+public class Carta
+{
+    string Nome { get; set; }
+    string Tipo { get; set; }
+
+    public Carta(string Nome, string Tipo)
+    {
+        this.Nome = Nome;
+        this.Tipo = Tipo;
+    }
+}
