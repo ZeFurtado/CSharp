7012d020defeb55713c3fbfa5db1f501d6218896
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-14 22:20:47 -0300

    Jogador

---
+++ OpenTK/Detetive/Jogador.cs
@@ -0,0 +1,13 @@
+﻿namespace Detetive;
+
+public class Jogador
+{
+    public string Nome { get; set; }
+    public List<Carta> Cartas { get; set; }
+
+    public Jogador(string nome, List<Carta> cartas)
+    {
+        Nome = nome;
+        Cartas = cartas;
+    }
+}
