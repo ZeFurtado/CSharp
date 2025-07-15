c6395833275b8910ddf1fe6e698050b564a3e080
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-14 22:21:19 -0300

    Personagem

---
+++ OpenTK/Detetive/Personagem.cs
@@ -0,0 +1,13 @@
+﻿namespace Detetive;
+
+public class Personagem
+{
+    public string Nome { get; set; }
+    public string Cor { get; set; }
+
+    public Personagem(string Nome, string Cor)
+    {
+        this.Nome = Nome;
+        this.Cor = Cor;
+    }
+}
