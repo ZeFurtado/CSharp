1eecf93594b0405fccb44bd408f1934372e392ca
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-16 06:07:40 -0300

    Program.cs

--- OpenTK/Detetive/Program.cs
+++ OpenTK/Detetive/Program.cs
@@ -3,7 +3,10 @@
 public class Program
 {
     static void Main(string[] args)
-    { 
-        
+    {
+        using (Game game = new Game(800, 600, "Janela"))
+        {
+            game.Run();
+        }
     }
 }
