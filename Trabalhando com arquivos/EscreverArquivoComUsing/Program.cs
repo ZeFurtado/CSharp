4aecbbbc62f37597058de4b6b41191d43239f95c
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-17 21:23:51 -0300

    EscreverArquivoComUsing

---
+++ Trabalhando com arquivos/EscreverArquivoComUsing/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EscreverArquivoComUsing;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        using (StreamWriter arquivo = new StreamWriter("/home/lukhas/Documentos/texto.txt"))
+        { 
+            try
+            {
+            arquivo.WriteLine("Tralalero Tralala");
+            arquivo.WriteLine("Bon giorno\n");
+
+            arquivo.Close();
+            Console.WriteLine("Arquivo gravado");
+            }
+            catch (Exception exception)
+            {
+            Console.WriteLine(exception.Message);
+            Console.WriteLine(exception.InnerException);
+            }
+        }
+        
+    }
+}
