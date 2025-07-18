0303b6b9062e7297616e619617b42cf6b3045a98
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-17 21:23:27 -0300

    Ler Arquivo

---
+++ Trabalhando com arquivos/LerArquivo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace LerArquivo;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = File.ReadAllText("/home/lukhas/Documentos/texto.txt");
+
+        Console.WriteLine(texto);
+
+        string[] linhas = File.ReadAllLines("/home/lukhas/Documentos/texto.txt");
+
+        foreach (string n in linhas)
+        {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine("Quantidade de linhas: " + linhas.Length); 
+        Console.ReadKey();
+    }
+
+    
+}
