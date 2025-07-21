0600762323be51f6b1a0f35f4c1c215e37bde607
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-21 01:18:17 -0300

    Projeto

---
+++ Projeto/Projeto/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("O que deseja fazer? \n(1) = Cadastrar Cliente \n(2) = Listar Clientes \n" +
+                          "(3) = Cadastrar funcionário \n(4) = Listar funcionário");
+        Console.ReadKey();
+    }
+}
