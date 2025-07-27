5051d18a83d5fadaf8843ec9cbd0cf0c6433d89d
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-27 17:44:31 -0300

    ManterCliente.cs

---
+++ Projeto/Projeto/Telas/ManterCliente.cs
@@ -0,0 +1,52 @@
+﻿using Biblioteca.Model;
+
+namespace Projeto.Telas;
+
+public class ManterCliente
+{
+    public void CadastrarCliente()
+    {
+        Cliente cliente = new Cliente();
+
+        Console.Clear();
+
+        Console.WriteLine("CADASTRAR CLIENTE");
+
+        Console.Write("Nome: ");
+        cliente.Nome = Console.ReadLine();
+
+
+        Console.WriteLine("Data de Nascimento: ");
+        cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Telefone: ");
+        cliente.Telefone = Console.ReadLine();
+
+        Console.WriteLine("CPF: ");
+        cliente.CPF = Console.ReadLine();
+
+        Console.WriteLine("RG: ");
+        cliente.RG = Console.ReadLine();
+
+        Console.WriteLine("CEP: ");
+        cliente.CEP = Console.ReadLine();
+
+        Console.WriteLine("Estado: ");
+        cliente.Estado = Console.ReadLine();
+
+        Console.WriteLine("Cidade: ");
+        cliente.Cidade = Console.ReadLine();
+
+        Console.WriteLine("Endereço: ");
+        cliente.Endereco = Console.ReadLine();
+
+        
+    }
+
+    public void ListarClientes()
+    {
+        Cliente cliente = new Cliente();
+        Console.WriteLine(cliente.Nome);
+    }
+
+}
