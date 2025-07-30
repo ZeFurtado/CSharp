b1ff69062874fa4304e09e9caea7ed29dcd4833a
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-29 23:08:02 -0300

    Bloco

---
+++ OpenTK/Detetive/Bloco.cs
@@ -0,0 +1,13 @@
+﻿namespace Detetive;
+
+public class Bloco
+{
+    string Localizacao { get; set; }
+    /* 
+    Ex:
+    A1 | A2 | A3 | A4 |
+    B1 | B2 | B3 | B4 |
+    C1 | C2 | C3 | C4 |
+     */
+    bool Ocupado { get; set; } = false; //Caso o jogador pare 'em cima' do bloco ele é setado Ocupado = true
+}
