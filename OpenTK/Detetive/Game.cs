08dc07c1b6c92eca76ebf3604cb3d41be9132926
ZeFurtado <lukhasfurtado9@gmail.com>
2025-07-29 23:15:48 -0300

    Game

---
+++ OpenTK/Detetive/Game.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace Detetive;
+
+public class Game : GameWindow
+{
+    public Game(int width, int height, string title) : base (GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title})
+    { 
+
+    }
+}
