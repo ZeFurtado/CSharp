54482580c41035da651a822db1d7d6a777d0bd76
ZeFurtado <lukhasfurtado9@gmail.com>
2025-08-05 21:18:23 -0300

    CEP alterado de tipo string para tipo int

--- Projeto/Biblioteca/Model/Pessoa.cs
+++ Projeto/Biblioteca/Model/Pessoa.cs
@@ -8,7 +8,7 @@ public abstract class Pessoa
     public string CPF { get; set; }
     public string RG { get; set; }
 
-    public string CEP { get; set; }
+    public int CEP { get; set; }
     public string Estado { get; set; }
     public string Cidade { get; set; }
     public string Endereco { get; set; }
