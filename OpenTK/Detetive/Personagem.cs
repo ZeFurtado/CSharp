e5456e879b13181b4e04142407ca90a10e8cc04d
ZeFurtado <lukhasfurtado9@gmail.com>
2025-08-16 23:16:23 -0300

    Personagem

--- OpenTK/Detetive/Personagem.cs
+++ OpenTK/Detetive/Personagem.cs
@@ -5,11 +5,12 @@ public class Personagem
     public string Nome { get; set; }
     public string Cor { get; set; }
 
-    public string PosInicial { get; set; }
+    public Bloco PosInicial { get; set; }
 
-    public Personagem(string nome, string cor)
+    public Personagem(string nome, string cor, Bloco posInicial)
     {
         Nome = nome;
         Cor = cor;
+        PosInicial = posInicial;
     }
 }
