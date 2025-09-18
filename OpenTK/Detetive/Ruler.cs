0556c93cc4ff2ab93ab1ea7bb20291ad8e45b5a6
ZeFurtado <lukhasfurtado9@gmail.com>
2025-09-18 20:10:01 -0300

    Criação do array de objetos Arma e altarado para inicial maiúscula o nome dos arrays

--- OpenTK/Detetive/Ruler.cs
+++ OpenTK/Detetive/Ruler.cs
@@ -1,9 +1,7 @@
 ﻿namespace Detetive;
 
-public class Ruler //Classe criada para carregar os objetos do jogo
+public static class Ruler //Classe criada para carregar os objetos do jogo
 {
-
-
     static Bloco[] tabuleiro =
     {
      new Bloco("A1"), new Bloco("A2"), new Bloco("A3"), new Bloco("A4"),
@@ -11,7 +9,7 @@ public class Ruler //Classe criada para carregar os objetos do jogo
      new Bloco("C1"), new Bloco("C2"), new Bloco("C3"), new Bloco("C4"),
     };
 
-    private Personagem[] personagens =
+    private static Personagem[] personagens =
     {
         new Personagem("Advogado Marinho", "Verde", tabuleiro[0]), new Personagem("Chefe Tony Gourmet", "Marrom", tabuleiro[1]),
         new Personagem("Médica Dona Violeta", "Roxo", tabuleiro[2]), new Personagem("Coveiro Sérgio Soturno", "Preto", tabuleiro[3]),
@@ -19,15 +17,38 @@ public class Ruler //Classe criada para carregar os objetos do jogo
         new Personagem("Sargento Bigode", "Amarelo", tabuleiro[6])
     };
 
+    private static Carta[] cartas =
+    {
+        new Carta("Tesoura", "Arma"), new Carta("Pé de Cabra", "Arma"), new Carta("Veneno", "Arma"),
+        new Carta("Soco Ingês", "Arma"), new Carta("Faca", "Arma"), new Carta("Espingarda", "Arma"),
+        new Carta("Pá", "Arma"), new Carta("Arma Química", "Arma")
+    };
 
-    public void ExibirTabuleiro()
-    { 
+
+    public static Personagem GetPersonagem(string nomePersonagem)
+    {
+        foreach (Personagem p in personagens)
+        {
+            if (p.Nome == nomePersonagem) return p;
+        }
+
+        return personagens[0];
+    }
+
+
+    public static void ExibirTabuleiro()
+    {
         for (int i = 0; i < tabuleiro.Count(); i++)
         {
             if (i == 4) Console.Write("\n");
             if (i == 8) Console.Write("\n");
             Console.Write($" {tabuleiro[i].Localizacao} |");
         }
+    }
+
+    public static void CadastroJogador()
+    {
+        
     }
     
 }
