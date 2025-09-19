d741f49c631cf118a5229c1739b31ef1e751e60c
ZeFurtado <lukhasfurtado9@gmail.com>
2025-09-18 21:00:21 -0300

    Arma.cs

---
+++ OpenTK/Detetive/Arma.cs
@@ -0,0 +1,11 @@
+﻿namespace Detetive;
+
+public class Arma
+{
+    public string Nome { get; set; }
+
+    public Arma(string nome)
+    {
+        Nome = nome;
+    }
+}
--- OpenTK/Detetive/Program.cs
+++ OpenTK/Detetive/Program.cs
@@ -7,34 +7,20 @@ public class Program
 {
     static void Main(string[] args)
     {
-
-        Bloco[] tabuleiro =
-        { new Bloco("A1"), new Bloco("A2"), new Bloco("A3"), new Bloco("A4"),
-          new Bloco("B1"), new Bloco("B2"), new Bloco("B3"), new Bloco("B4"),
-          new Bloco("C1"), new Bloco("C2"), new Bloco("C3"), new Bloco("C4"),
-        };
-
-        Jogador jogador1 = new Jogador("Jogador1", new Personagem ("Sgt", "Verde", tabuleiro[0]));
-
-
-        for (int i = 0; i < tabuleiro.Count(); i++)
-        {
-            if (i == 4) Console.Write("\n");
-            if (i == 8) Console.Write("\n");
-            Console.Write($" {tabuleiro[i].Localizacao} |");
-        }
-
+        string nomeJogador;
+        
         int opcao = 1;
         while (opcao != 0)
         {
-            Console.WriteLine("1- Jogar dado     2- Sair");
+            Console.WriteLine("1- Começar jogo     2- Sair");
             opcao = int.Parse(Console.ReadLine());
 
 
             switch (opcao)
             {
                 case 1:
-                    jogador1.JogarDado();
+                    Console.WriteLine("Digite seu nome: ");
+                    nomeJogador = Console.ReadLine();
                     break;
 
                 case 2:
@@ -50,8 +36,6 @@ public class Program
             
 
         }
-        
-        
 
 
 
@@ -61,9 +45,6 @@ public class Program
             game.Run();
         }
          */
-
-
-
 
     }
 }
