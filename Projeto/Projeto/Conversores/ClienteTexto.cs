589f93cf852405e01e5288c6b64c9f9933132a63
ZeFurtado <lukhasfurtado9@gmail.com>
2025-11-02 02:05:16 -0300

    Atribuição da propriedade DataNascimento agora feita através do método set

--- Projeto/Projeto/Conversores/ClienteTexto.cs
+++ Projeto/Projeto/Conversores/ClienteTexto.cs
@@ -7,7 +7,7 @@ public class ClienteTexto
     public static string ConverterParaTexto(Cliente cliente)
     {
         string texto = String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}",
-                        cliente.Nome, cliente.DataNascimento, cliente.Telefone,
+                        cliente.Nome, cliente.getDataNascimento().ToString(), cliente.Telefone,
                         cliente.CPF, cliente.RG, cliente.CEP, cliente.Estado, cliente.Cidade,
                         cliente.Endereco);
         return texto;
@@ -19,7 +19,7 @@ public class ClienteTexto
 
         Cliente cliente = new Cliente();
         cliente.Nome = elementos[0];
-        cliente.DataNascimento = DateTime.Parse(elementos[1]);
+        cliente.setDataNascimento(elementos[1]);
         cliente.Telefone = elementos[2];
         cliente.CPF = elementos[3];
         cliente.RG = elementos[4];
