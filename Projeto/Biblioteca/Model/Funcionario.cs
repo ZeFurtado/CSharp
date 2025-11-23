c7202e4db182d36911499ddb1c1ad45ebc4695bb
ZeFurtado <lukhasfurtado9@gmail.com>
2025-11-23 11:24:14 -0300

    Implementado validação do formato da data

--- Projeto/Biblioteca/Model/Funcionario.cs
+++ Projeto/Biblioteca/Model/Funcionario.cs
@@ -5,5 +5,24 @@ public sealed class Funcionario : Pessoa
     public string Cargo { get; set; }
     public double Salario { get; set; }
 
-    public DateTime DataContratacao { get; set; }
+    private DateTime DataContratacao;
+
+    public DateTime getDataContracao()
+    {
+        return DataContratacao;
+    }
+
+    public void setDataContratacao(string dataContratacao)
+    {
+        try
+        {
+            DataContratacao = DateTime.Parse(dataContratacao);
+        }catch(FormatException)
+        {
+            Console.WriteLine("O formato da data é inválido.");
+            Console.WriteLine("Data da Contratação: ");
+            setDataContratacao(Console.ReadLine());
+        }
+    }
+
 }
