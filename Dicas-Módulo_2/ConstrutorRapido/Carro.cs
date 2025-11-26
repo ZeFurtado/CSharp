179b3d0b54e8247761ce2aa1c19ca37ab21711e8
ZeFurtado <lukhasfurtado9@gmail.com>
2025-11-26 19:58:14 -0300

    Construtor rápido

---
+++ Dicas-Módulo_2/ConstrutorRapido/Carro.cs
@@ -0,0 +1,10 @@
+namespace ConstrutorRapido;
+
+public class Carro
+{
+    public string Marca{get; set;}
+    public string Modelo{get; set;}
+    public int Rodas{get; set;}
+    public DateTime DataFabricacao{get; set;}
+    
+}
---
+++ Dicas-Módulo_2/ConstrutorRapido/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConstrutorRapido;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Construtor Padrão
+        Carro carro = new Carro();
+
+        //Construtor Rápido
+        Carro carro2 = new Carro() {Marca = "FIAT", Rodas = 4, DataFabricacao = new DateTime(2000,10,10), Modelo="Palio"};
+    }
+}
