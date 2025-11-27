84d7ea868ea166e976ad949673a07a8a314362c2
ZeFurtado <lukhasfurtado9@gmail.com>
2025-11-26 21:16:47 -0300

    UsingStatic

---
+++ Dicas-Módulo_2/UsingStatic/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Math; 
+
+namespace UsingStatic;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Utilizando a diretiva no topo do código torna se desnecessário o uso do Math
+        Math.Pow(2, 3);
+    }
+}
