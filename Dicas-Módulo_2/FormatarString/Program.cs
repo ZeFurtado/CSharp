458dd0dc9758fd2406abec5bc37f9fb5556845c0
ZeFurtado <lukhasfurtado9@gmail.com>
2025-11-26 21:43:26 -0300

    Formatar string

---
+++ Dicas-Módulo_2/FormatarString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FormatarString;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = String.Format("Olá {0}! Feliz {1}!", "Elias", "2017");
+        Console.WriteLine(texto);
+
+        string nome = "Elaias", ano = "2017";
+        Console.WriteLine("Olá {0}! Feliz {1}!", nome, ano);
+
+        Console.WriteLine("Olá ({nome})! Feliz ({ano})!");
+
+        Console.WriteLine($"Olá {nome}! Feliz {ano}!");
+        Console.ReadKey();
+    }
+}
