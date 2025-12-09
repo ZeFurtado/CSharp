369a028a56d815b27cc5ec3c34d9883aed225bb3
ZeFurtado <lukhasfurtado9@gmail.com>
2025-12-09 06:05:43 -0300

    Criado classe de imc para teste

--- Serializacao/00_Biblioteca/IMC.cs
+++ Serializacao/00_Biblioteca/IMC.cs
@@ -1,7 +1,14 @@
+using System.Security;
+
 namespace _00_Biblioteca;
 
 public class IMC
 {
     public int Peso { get; set; }
     public double Altura {get;set;}
+
+    public double getImc()
+    {
+        return Peso / (Altura * Altura);
+    }
 }
