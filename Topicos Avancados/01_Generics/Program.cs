eac621ef4ee22021a8a1a890d58a9a101a4c4938
ZeFurtado <lukhasfurtado9@gmail.com>
2025-12-16 22:32:08 -0300

    01_Generics

--- Topicos Avancados/01_Generics/Program.cs
+++ Topicos Avancados/01_Generics/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace _01_Generics;
+﻿using System.Threading.Tasks;
+using _01_Generics.modelos;
+
+namespace _01_Generics;
 
 public class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        
+        Carro carro = new Carro(){Marca = "Chrevolet", Modelo="Celta"};
+        Casa casa = new Casa(){Cidade="Ivoti", Endereco="Boa Vista"};
+        Usuario usuario = new Usuario(){Nome = "Júlio", Email="julio@gmail.com", Senha="9770607060"};
+
+        await Serializador.Serializar(carro);
+        await Serializador.Serializar(usuario);
+        await Serializador.Serializar(casa);
+
+        Serializador.LerArquivo(carro);
+
+
+        Carro carro2 = Serializador.Deserializar<Carro>();
+        Usuario usuario2 = Serializador.Deserializar<Usuario>();
+        Casa casa2 = Serializador.Deserializar<Casa>();
+
+        Console.WriteLine("Carro: Marca: {0} | Modelo: {1}", carro2.Marca, carro2.Modelo);
+        Console.WriteLine("Usuário: Nome: {0} | E-mail: {1} | Senha: {2}", usuario2.Nome, usuario2.Email, usuario2.Senha);
+        Console.WriteLine("Casa: Cidade: {0} |  Endereço: {1}", casa2.Cidade, casa2.Endereco);
     }
 }
---
+++ Topicos Avancados/01_Generics/Serializador.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace _01_Generics;
+
+public class Serializador 
+{
+    public static async Task Serializar(object obj)
+    {
+        try
+        {
+            string path = $"/home/lukhas/repo/CSharp/Topicos Avancados/{obj.GetType().Name}JsonFile.json";
+
+            StreamWriter streamWriter = new StreamWriter(path);
+            string texto = JsonSerializer.Serialize(obj, obj.GetType());
+            streamWriter.WriteLine(texto);
+
+            streamWriter.Close();
+            
+        }catch (Exception ex)
+        {
+            Console.WriteLine("Mensagem de Erro IO: " + ex.Message + "Fonte:  " + ex.Source);
+        }
+    }
+
+    public static void LerArquivo(object obj)
+    {
+        try
+        {
+            string path = $"/home/lukhas/repo/CSharp/Topicos Avancados/{obj.GetType().Name}JsonFile.json";
+            if(!File.Exists(path))throw new IOException();
+
+            StreamReader streamReader = new StreamReader(path);
+            string texto = streamReader.ReadToEnd();
+            streamReader.Close();
+            
+            Console.WriteLine(texto); 
+        }catch(IOException ex)
+        {
+            Console.WriteLine("Mensagem de Erro IO: " + ex.Message + "Fonte:  " + ex.Source);
+        }
+            
+    }
+
+    public static T? Deserializar<T>()
+    {
+        try
+        {
+            string jsonFile = File.ReadAllText($"/home/lukhas/repo/CSharp/Topicos Avancados/{typeof(T).Name}JsonFile.json");
+            T t = JsonSerializer.Deserialize<T>(jsonFile);
+            return t;
+        }catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return default(T);
+        }
+    }
+}
