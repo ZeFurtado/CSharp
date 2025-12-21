0c92aabc0b1eba3443223a09516ea69792bd034b
ZeFurtado <lukhasfurtado9@gmail.com>
2025-12-20 23:26:39 -0300

    Criação do Objetos em andamento

--- OpenTK/Detetive/Program.cs
+++ OpenTK/Detetive/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using OpenTK.Graphics.OpenGL;
+using Detetive.Classes;
 
 namespace Detetive;
 
@@ -7,20 +8,33 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string nomeJogador;
-        
+
+        Bloco[] tabuleiro =
+        { new Bloco("A", 1), new Bloco("A", 2), new Bloco("A",3), new Bloco("A",4),
+          new Bloco("B", 1), new Bloco("B", 2), new Bloco("B",3), new Bloco("B",4),
+          new Bloco("C",1), new Bloco("C",2), new Bloco("C",3), new Bloco("C",4),
+        };
+
+        Jogador jogador1 = new Jogador("Jogador1", new Personagem ("Sgt", "Verde", tabuleiro[0]));
+
+
+        for (int i = 0; i < tabuleiro.Count(); i++)
+        {
+            if (i == 4) Console.Write("\n");
+            if (i == 8) Console.Write("\n");
+            Console.Write($" {tabuleiro[i].Localizacao} |");
+        }
+
         int opcao = 1;
         while (opcao != 0)
         {
-            Console.WriteLine("1- Começar jogo     2- Sair");
+            Console.WriteLine("1- Jogar dado     2- Sair");
             opcao = int.Parse(Console.ReadLine());
-
 
             switch (opcao)
             {
                 case 1:
-                    Console.WriteLine("Digite seu nome: ");
-                    nomeJogador = Console.ReadLine();
+                    jogador1.JogarDado();
                     break;
 
                 case 2:
@@ -36,6 +50,8 @@ public class Program
             
 
         }
+        
+        
 
 
 
@@ -45,6 +61,9 @@ public class Program
             game.Run();
         }
          */
+
+
+
 
     }
 }
