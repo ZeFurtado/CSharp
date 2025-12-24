bb68e868be82ddda3e07044506786e3a21c76c8b
ZeFurtado <lukhasfurtado9@gmail.com>
2025-12-24 19:55:33 -0300

    02_VarDynamic

---
+++ Topicos Avancados/02_VarDynamic/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+
+namespace _02_VarDynamic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        object obj = "123456";
+        object obj2 = 123456;
+        object obj3 = new Usuario(){Nome =  "Josefino"};
+        Console.WriteLine(((Usuario)obj3).Nome);
+
+
+        var vi = "";
+        var v2 = 123456;
+        // var v3; //Necessária declaração do tipo de variável
+
+        dynamic d1 = new Usuario {Nome="Maria"};
+
+        try
+        {
+            Console.WriteLine(d1.Nome);
+            Console.WriteLine(d1.Senha);
+        }catch (RuntimeBinderException bEx)
+        {
+            Console.WriteLine(bEx.Message);
+        }
+
+    }
+
+    class Usuario
+    {
+        public string Nome { get; set; }
+    }
+}
