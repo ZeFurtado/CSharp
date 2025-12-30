a24a4d25fb3599625db65d4a23f8330315984141
ZeFurtado <lukhasfurtado9@gmail.com>
2025-12-30 10:51:59 -0300

    03_Extension_Methods

---
+++ Topicos Avancados/03_Extension_Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_Extension_Methods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = "olá mundo!";
+        Console.WriteLine(texto.FirsTToUpper());
+    }
+}
---
+++ Topicos Avancados/03_Extension_Methods/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.Contracts;
+using System.Threading.Tasks.Sources;
+
+namespace System;
+
+public static class StringExtension
+{   
+    public static string FirsTToUpper(this string str)
+    {
+        string primeiraLetra =  str.Substring(0,1).ToUpper();
+        string segundaParte = str.Substring(1, str.Length - 1);   
+
+        return  primeiraLetra + segundaParte;
+    }
+}
