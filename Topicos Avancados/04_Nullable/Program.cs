878e71a6bb3084df640f8b58d2bf7fdd038d69a1
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-06 20:02:28 -0300

    Nullable

---
+++ Topicos Avancados/04_Nullable/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _04_Nullable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Struct - Por Valor
+        int i = null;
+
+        //Class - Por referência
+        object o = null;
+
+        //MySql - Oracle - PostGreeSQL ... NoSQL
+        Int32 idade = null
+        Nullable<int> idade2 = null;
+
+        int? idade3 = null;
+    }
+}
