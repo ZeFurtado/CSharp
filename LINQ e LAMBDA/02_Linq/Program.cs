2aad62686ba2eacab064c8bf7b57a1d07672f25e
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-07 19:48:08 -0300

    02_Linq

---
+++ LINQ e LAMBDA/02_Linq/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02_Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Usuario> usuarios = new List<Usuario>();
+        
+        usuarios.Add(new Usuario{Nome = "Sérgio", Email = "sergio@yahoo.com"});
+        usuarios.Add(new Usuario{Nome = "Roberto", Email = "roberto@gmail.com"});
+        usuarios.Add(new Usuario{Nome = "Alberto", Email = "alberto@outlook.com"});
+        usuarios.Add(new Usuario{Nome = "Eloi", Email="eloi@gmail.com"});
+        usuarios.Add(new Usuario{Nome="Marcio", Email="marcio@gmail.com"});
+
+
+        var listaFiltrada = usuarios.Where(a => a.Email.Contains("@gmail")).Select(a => a);
+
+        foreach(var item in listaFiltrada)
+        {
+            Console.WriteLine("Nome: {0}     Email: {1}", item.Nome, item.Email);        
+        }
+    }
+}
---
+++ LINQ e LAMBDA/02_Linq/Usuario.cs
@@ -0,0 +1,8 @@
+namespace _02_Linq;
+
+public class Usuario
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+
+}
