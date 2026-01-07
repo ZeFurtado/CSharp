d786aa9c5428ca5fd916e364c4a00e3b1239fc37
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-07 20:02:27 -0300

    Livro.cs

---
+++ LINQ e LAMBDA/03_Linq_Join/Livro.cs
@@ -0,0 +1,10 @@
+namespace _03_Linq_Join;
+
+public class Livro
+{
+    public int Id {get;set;}
+    public int AutorId{get;set;}
+    public string Titulo { get; set; }
+    public string AnoPublicacao { get; set; }
+
+}
