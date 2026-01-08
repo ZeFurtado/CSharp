6d8fe4a8baa4881c99ead84563428644e1567f83
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-08 20:20:58 -0300

    01_Delegate

---
+++ Delegate e Eventos/01_Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _01_Delegate;
+
+class Program
+{
+
+    delegate int Calcula(int a, int b);
+    static void Main(string[] args)
+    {
+
+        Calcula calc = new Calcula(soma);
+        var so = calc(10,20);
+
+        Calcula calc2 = new Calcula(subtrair);
+        var su = calc2(10,20);
+
+        Console.WriteLine("Soma: {0}   Subtração: {1}", so, su);
+
+        /*
+        int so = soma(10,1);
+        int su = subtrair(10,1);
+        */
+    }
+
+    static int soma(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int subtrair(int a, int b)
+    {
+        return a - b;
+    }
+}
