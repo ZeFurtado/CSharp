6067bbca0383d2a59c8cb4a055214004106ce59f
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-08 22:18:46 -0300

    02_Delegates

---
+++ Delegate e Eventos/02_Delegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using _02_Delegates.Lib;
+
+namespace _02_Delegates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //Tela - Cadastro de usuário: Thumb (foto de perfil)
+        Foto fotoUsuario = new Foto{Nome = "fotoUsuario.jpg", TamanhoX=1920, TamanhoY=1080};
+        FotoProcessador.filtros = new FotoFiltro().GerarThumb;
+        FotoProcessador.Processar(fotoUsuario);
+        
+        //Tela - Cadastro de Produtos: Colorir + TamanhoMed
+
+        //Tela - Cadastro de Albuns do Usuário - Retro: Preto e Branco
+        
+    }
+}
