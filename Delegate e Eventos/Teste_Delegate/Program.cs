f34a90720fa8a149b616f9be9a9c3123d9894b70
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-10 01:35:32 -0300

    Teste_Delegate

---
+++ Delegate e Eventos/Teste_Delegate/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Teste_Delegate;
+
+class Program
+{
+    delegate void PrintDelegate (string text);
+    static void Main(string[] args)
+    {
+        //Printar na tela
+        ConectarBaseDeDados(PrintaNaTela);
+
+        //SalvarArquivo
+        ConectarBaseDeDados(PrintaNoArquivo);
+    }
+
+    static PrintDelegate PrintaNaTela = (string text) =>
+    {
+        Console.WriteLine("Printou na tela {0}", DateTime.Now);
+    };
+
+    static PrintDelegate PrintaNoArquivo = (string text) =>
+    {
+        File.AppendAllText("/home/lukhas/Documentos/log.txt", text);
+    };
+
+    static void ConectarBaseDeDados(PrintDelegate log)
+    {
+        //Tentando Conectar
+        log($"Testando conexão........{DateTime.Now}");
+        Thread.Sleep(1500);
+        //Primeira Tentativa
+        log($"Conexão falhou!Tentando novamente........{DateTime.Now}");
+        Thread.Sleep(3000);
+        //Segunda tentativa
+        log($"Conexão bem sucedida! {DateTime.Now}");
+
+    }
+}
