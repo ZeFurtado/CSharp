9303eed45fab6a59ec7471e7a670a80161107b97
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-13 21:30:20 -0300

    Criada função números inteiros

--- Delegate e Eventos/Exercicio_Delegate/Inteiros.cs
+++ Delegate e Eventos/Exercicio_Delegate/Inteiros.cs
@@ -97,10 +97,41 @@ public class Inteiros
         return arrayRetorno;
     }
 
-    /*public int[] NumerosPrimos(int[] array)
+    public static int[] NumerosPrimos(int[] array)
     {
         int[] arrayTemp = new int[array.Count()];
         int p = -1;
-    }*/
+
+        foreach (int i in array)
+        {
+            if(EhPrimo(i))
+            {
+              p++; 
+              arrayTemp[p] = i;  
+            }   
+        }
+
+        int[] arrayRetorno = new int[p + 1];
+
+        for(int q = 0; q < p + 1; q++)
+        {
+           arrayRetorno[q] = arrayTemp[q];
+        }
+
+        return arrayRetorno;
+    }
+
+    private static bool EhPrimo(int i)
+    {
+        if(i < 2) return false;
+        if(i == 2 || i == 3) return true;
+        
+        for(int n = 2; n <= (int)Math.Sqrt(i); n++)
+        {
+            if(i % n == 0) return false;
+        }
+
+        return true;
+    }
 
 }
