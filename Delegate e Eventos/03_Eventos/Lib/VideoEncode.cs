ea1ef539019b1b34c028b62503278ec98cdeb6e2
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-15 22:35:51 -0300

    Delegate ----> Event

--- Delegate e Eventos/03_Eventos/Lib/VideoEncode.cs
+++ Delegate e Eventos/03_Eventos/Lib/VideoEncode.cs
@@ -3,14 +3,21 @@ namespace _03_Eventos.Lib;
 
 public class VideoEncode
 {
-    public delegate void videoEncodedHandler(Video video);
-    public event videoEncodedHandler Encoded;
+    //public delegate void videoEncodedHandler(Video video);
+    //public event videoEncodedHandler Encoded;
+
+    public event EventHandler<VideoEventArgs> Encoded;
     public void Encode(Video video)
     {
         Console.WriteLine("Convertendo o vídeo...");
         Thread.Sleep(2000);
         Console.WriteLine("Vídeo convertido!");
 
-        Encoded(video);
+        Encoded(this, new VideoEventArgs(){Video = video});
+    }
+
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
     }
 }
