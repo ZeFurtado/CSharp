78b416f878fb0ec6528a502763d8929eff764110
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-15 22:40:16 -0300

    Referência as classes do diretório Mensageiro e chamada do método EnviarMensagem da classe SMS e Email

--- Delegate e Eventos/03_Eventos/Program.cs
+++ Delegate e Eventos/03_Eventos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _03_Eventos.Lib;
+using _03_Eventos.Lib.Mensageiro;
 
 namespace _03_Eventos;
 
@@ -7,5 +8,14 @@ class Program
     static void Main(string[] args)
     {
         VideoEncode videoEncode = new VideoEncode();
+
+        Video video1 = new Video{Nome = "Momento 37"};
+
+        videoEncode.Encoded += new Email().EnviarMensagem;
+
+        videoEncode.Encoded += new SMS().EnviarMensagem;
+
+        videoEncode.Encode(video1);
+
     }
 }
