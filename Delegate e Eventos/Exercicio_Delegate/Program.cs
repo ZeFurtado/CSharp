52af03087a022e085de98a82a714bb5c1d24247d
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-16 23:36:42 -0300

    Adicionado array padrão para testes

--- Delegate e Eventos/Exercicio_Delegate/Program.cs
+++ Delegate e Eventos/Exercicio_Delegate/Program.cs
@@ -1,13 +1,16 @@
-﻿namespace Exercicio_Delegate;
+﻿using System.Reflection;
+
+namespace Exercicio_Delegate;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        int[] array1 = new int[]{10,14,5,2,3,9,7,17,65,100,45,103,199};
         Random random = new Random(); 
         int[] inteiros = new int[random.Next(100, 1000)];
         
-        for(int x = 0; x < inteiros.Count(); x++)
+        for(int x = 0; x < array1.Count(); x++)
         {
             inteiros[x] = random.Next(0, 8000); 
         }
@@ -23,9 +26,5 @@ public class Program
             Console.WriteLine(i);
         }
         
-    }
-
-    
-
-    
+    }    
 }
