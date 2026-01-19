936582ae83f991af9ec8fb8b6b65f5cc680b28be
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-19 19:28:50 -0300

    utilizando array inteiros ao invés do array1

--- Delegate e Eventos/Exercicio_Delegate/Program.cs
+++ Delegate e Eventos/Exercicio_Delegate/Program.cs
@@ -6,17 +6,16 @@ public class Program
 {
     static void Main(string[] args)
     {
-        int[] array1 = new int[]{10,14,5,2,3,9,7,17,65,100,45,103,199};
         Random random = new Random(); 
         int[] inteiros = new int[random.Next(100, 1000)];
         
-        for(int x = 0; x < array1.Count(); x++)
+        for(int x = 0; x < inteiros.Count(); x++)
         {
             inteiros[x] = random.Next(0, 8000); 
         }
         
         Console.WriteLine("---Array orginal: ");
-        foreach(int i in array1)
+        foreach(int i in inteiros)
         {
             Console.WriteLine(i);
         }
@@ -32,7 +31,7 @@ public class Program
         Ordenar.inteirosHandler += Inteiros.NumerosImpares;
         Ordenar.inteirosHandler += Inteiros.MenorQueCem;
 
-        int[] arrayOrdenado = Ordenar.Ordena(ref array1);
+        int[] arrayOrdenado = Ordenar.Ordena(ref inteiros);
         //Ordenar.inteirosHandler += Inteiros.MenorQueCem;
         //Ordenar.inteirosHandler += Inteiros.NumerosPrimos;
 
