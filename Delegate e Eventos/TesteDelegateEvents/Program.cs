988396cc0b7075b15360d83651bd244b92d224fa
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-19 21:54:31 -0300

    Implementada a chamada e exibição do resultado dos métodos

--- Delegate e Eventos/TesteDelegateEvents/Program.cs
+++ Delegate e Eventos/TesteDelegateEvents/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     static void Main(string[] args)
     {
-        
+        Usuario josir = new Usuario("Josir", 13, "Gerente Geral");
+
+        LidaComEvento lidaComEvento = new LidaComEvento();
+
+        lidaComEvento.mensagem += Mensagem.Mensagem1;
+        lidaComEvento.mensagem += Mensagem.Mensagem2;
+        lidaComEvento.mensagem += Mensagem.Mensagem3;
+
+        lidaComEvento.MensagemHandler(josir);
     }
 }
