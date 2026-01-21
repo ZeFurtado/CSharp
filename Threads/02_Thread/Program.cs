d2b92ed27a29388bf928af1b72b93fc8e40616b6
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-20 22:07:20 -0300

    02_Thread

---
+++ Threads/02_Thread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace _02_Thread;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(ThreadRepeticao);
+        thread.Start();
+
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("Main: "+ i);
+        }
+        Console.ReadKey();
+    }
+
+    public static void ThreadRepeticao()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("num: "+i);
+        }
+    }
+}
