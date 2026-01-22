65eb820e41edc4730f94f933a7266d462305d43b
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-22 20:38:47 -0300

    Adicionado linha de console para exibir horário de inicio da thread e final dela

--- Threads/04_ThreadBackground/Program.cs
+++ Threads/04_ThreadBackground/Program.cs
@@ -2,8 +2,27 @@
 
 public class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Hello");
+
+        Console.WriteLine("Início: ", DateTime.Now);
+
+        for(int i = 0; i < 6; i++)
+        {
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.IsBackground = true;
+            t1.Start();
+        }
+
+    }
+
+    static void ThreadRepeticao()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("Num:  " + i);
+        }
+
+        Console.WriteLine("Fim: " + DateTime.Now);
     }
 }
