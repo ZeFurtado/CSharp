20046c6dbc0e57898a3a02c67711388fc05f370c
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-22 21:24:44 -0300

    05_ThreadSafe

--- Threads/05_ThreadSafe/Program.cs
+++ Threads/05_ThreadSafe/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Início: ", DateTime.Now);
+
+        for(int i = 0; i < 5; i++)
+        {
+            Thread t1 = new Thread(ThreadRepeticao);
+            t1.IsBackground = true;
+            t1.Start();
+        }
+
+    }
+
+    static void ThreadRepeticao()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("Num:  " + i);
+        }
+
+        Console.WriteLine("Fim: " + DateTime.Now);
     }
 }
