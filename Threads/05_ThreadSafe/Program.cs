a7d607b9a1c4a99a452c4ff9b73eb20257b71faf
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-22 22:48:56 -0300

    Variável rede e variável de controle

--- Threads/05_ThreadSafe/Program.cs
+++ Threads/05_ThreadSafe/Program.cs
@@ -2,27 +2,32 @@
 
 class Program
 {
+    static int Rede = 0;
+    static object variavelDeControle = 0;
     static void Main(string[] args)
     {
-
         Console.WriteLine("Início: ", DateTime.Now);
-
+        
         for(int i = 0; i < 5; i++)
         {
             Thread t1 = new Thread(ThreadRepeticao);
             t1.IsBackground = true;
             t1.Start();
         }
-
+        Console.ReadKey();
     }
 
     static void ThreadRepeticao()
     {
-        for(int i = 0; i < 1000; i++)
-        {
-            Console.WriteLine("Num:  " + i);
-        }
 
+        lock(variavelDeControle)
+        {
+            for(int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Num:  " + i);
+                Rede++;
+            }
+        }
         Console.WriteLine("Fim: " + DateTime.Now);
     }
 }
