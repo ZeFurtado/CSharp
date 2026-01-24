524d7f77bf37b35167f397e5aff9468c7bcdc6a9
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-24 01:06:33 -0300

    Criadas funções ExecutarSequencial() e ExecutarComThreads()

--- Threads/ProgAssincrona/Program.cs
+++ Threads/ProgAssincrona/Program.cs
@@ -16,6 +16,7 @@ class Program
         stopwatch.Stop();
 
         Console.WriteLine($"Operação gastou: {stopwatch.ElapsedMilliseconds} milisegundos");
+        Console.ReadKey();
     }
 
     static void RealizarOperacao(int op, string nome, string sobrenome)
@@ -27,5 +28,30 @@ class Program
         }
 
         Console.WriteLine($"Finalizando operação {op}...");
+    }
+
+    static void ExecutarSequencial()
+    {
+        RealizarOperacao(1, "Joseph", "Griffin");
+        RealizarOperacao(2, "Jack", "Morgan");
+        RealizarOperacao(3, "Johnson", "Junior");
+    }
+
+    static void ExecutarComThreads()
+    {
+        var t1 = new Thread(() =>
+        {
+            RealizarOperacao(1, "Eduardo", "Moreira");
+        });
+
+        var t2 = new Thread(() =>
+        {
+            RealizarOperacao(2, "Getúlio", "Nargas");
+        });
+
+        var t3 = new Thread(() =>
+        {
+            RealizarOperacao(3, "Fidalgo", "Castro");
+        });
     }
 }
