54b5cc12475bbf60cd585c97d43f88fed48b45aa
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-25 21:38:07 -0300

    RepeticaoThread()

--- Threads/07_ThreadMetodos/Program.cs
+++ Threads/07_ThreadMetodos/Program.cs
@@ -4,8 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Thread.Sleep
         Console.WriteLine("Início: {0}", DateTime.Now);
         Thread.Sleep(3000 /*Milisegundos*/ ); //Determina quanto tempo a thread pode esperar para continuar
         Console.WriteLine("Fim: {0}", DateTime.Now);
+
+        //Thread Join
+        Thread t1 = new Thread(RepeticaoThread);
+        t1.Start();
+        t1.Join();//Espear o fim da execução da primeira thread para prosseguir
+
+        Console.WriteLine("Término do programa!");
+        Console.WriteLine("Término do programa!");
+        Console.WriteLine("Término do programa!");
+        Console.WriteLine("Término do programa!");
+
+        Console.ReadKey();
     }
+
+    static void RepeticaoThread(object ID)
+    { 
+        for(int i = 0; i < 1000; i++)
+        {
+            Thread.Sleep(5);
+            Console.WriteLine("Thread ID: {0}  - Num: {1}    - ID interno: {2}", ID, i, Thread.CurrentThread.ManagedThreadId);
+        }
+    } 
+    
 }
