8ef005e70acc4d1fdecc633c4626c21110caaebb
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-25 21:10:23 -0300

    Exibindo ID da thread no método RepticaoThread()

--- Threads/06_ThreadID/Program.cs
+++ Threads/06_ThreadID/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace _06_ThreadID;
+﻿using System.Diagnostics;
+
+namespace _06_ThreadID;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stopwatch stopwatch = new Stopwatch();
+
+        Console.WriteLine("Início: {0}", DateTime.Now);
+        stopwatch.Start();
+
+        for(int i =0; i < 5; i++)
+        {
+            var tM = new Thread(RepeticaoThread);
+            tM.IsBackground = true;
+            tM.Start(i);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine("A aplicação durou: {0} milisegundos!", stopwatch.ElapsedMilliseconds);
+        
+        Console.ReadKey();
     }
+
+
+
+    static void RepeticaoThread(object ID)
+    {
+        
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("Thread ID: {0}  - Num: {1}    - ID interno: {2}", ID, i, Thread.CurrentThread.ManagedThreadId);
+        }
+        
+    } 
 }
