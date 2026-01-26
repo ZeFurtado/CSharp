91554f1128dd076db02e173d9fad85299467d472
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-25 21:16:14 -0300

    07_ThreadMetodos

---
+++ Threads/07_ThreadMetodos/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _07_ThreadMetodos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Início: {0}", DateTime.Now);
+        Thread.Sleep(3000 /*Milisegundos*/ ); //Determina quanto tempo a thread pode esperar para continuar
+        Console.WriteLine("Fim: {0}", DateTime.Now);
+    }
+}
