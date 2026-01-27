2948d1ca49b587e783444d91aee0b6e69b4f795e
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-27 19:57:25 -0300

    Chamado do método através de threads e mudança no método de download da string

--- Tasks/01_Task/Program.cs
+++ Tasks/01_Task/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 
 namespace _01_Task;
 
@@ -6,23 +7,39 @@ class Program
 {
     static void Main(string[] args)
     {
+
         string endereco = "https://";
         Console.WriteLine("Baixador de HTML: ");
         Console.WriteLine("Digite a url completa do site que deseja baixar {0}", endereco );
         endereco += Console.ReadLine();
 
+        Button button = new Button(endereco);
         Console.WriteLine(endereco);
 
         Button.botaoHandler += DisparaMensagem;
-        Button.Disparar(endereco);    
+        //Button.Disparar(endereco);
+
+        Thread t1 = new Thread(button.Disparar); 
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        t1.Start(); 
+        
+        Thread.Sleep(2000);
+        stopwatch.Stop();
+
+        Console.WriteLine("Operação durou: {0} milisegundos", stopwatch.ElapsedMilliseconds);
+        
     }
 
 
 
-    static void DisparaMensagem(string endereco)
+    static async void DisparaMensagem(string endereco)
     {
         WebClient web = new WebClient();
-        string html = web.DownloadString(endereco);
+        //string html = web.DownloadString(endereco);
+        
+        string html = await web.DownloadStringTaskAsync(new Uri(endereco));
 
         Console.WriteLine(html);
     }
