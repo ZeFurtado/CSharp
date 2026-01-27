f3aad4fc629c5750b6b257612a7fe05640d5cc13
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-27 20:53:32 -0300

    02_TaskExecute

---
+++ Tasks/02_TaskExecute/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02_TaskExecute;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task.Run(() => Metodo01());
+        Task.Run(() =>  Metodo01());
+        Task.Factory.StartNew(Metodo01);
+        Console.ReadKey();
+    }
+
+    static void Metodo01()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine("Valor: "+ i + " ID Tarefa: " + Task.CurrentId);
+        }
+    }
+}
