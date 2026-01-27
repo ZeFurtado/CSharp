368fbc83f0e08d05651444b65c7349389842ce5b
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-27 19:56:29 -0300

    Criado construtor e sobrecarga de método Dispara()

--- Tasks/01_Task/Button.cs
+++ Tasks/01_Task/Button.cs
@@ -3,10 +3,21 @@ namespace _01_Task;
 
 public class Button
 {
+
+    public string Endereco{get;set;}
     public delegate void Botao(string endereco);
 
     public static event Botao botaoHandler;
 
+    public Button(string endereco)
+    {
+        Endereco = endereco;
+    }
+
+    public void Disparar()
+    {
+        botaoHandler(Endereco);
+    }
 
     public static void Disparar(string endereco)
     {
