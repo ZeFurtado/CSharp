1c3743f1adbb5b36248898167a99b1b98fe88f1a
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-26 21:48:26 -0300

    Implementada solução para baixar string de página web utilizando evento

--- Tasks/01_Task/Program.cs
+++ Tasks/01_Task/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace _01_Task;
+﻿using System.Net;
+
+namespace _01_Task;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string endereco = "https://";
+        Console.WriteLine("Baixador de HTML: ");
+        Console.WriteLine("Digite a url completa do site que deseja baixar {0}", endereco );
+        endereco += Console.ReadLine();
+
+        Console.WriteLine(endereco);
+
+        Button.botaoHandler += DisparaMensagem;
+        Button.Disparar(endereco);    
+    }
+
+
+
+    static void DisparaMensagem(string endereco)
+    {
+        WebClient web = new WebClient();
+        string html = web.DownloadString(endereco);
+
+        Console.WriteLine(html);
     }
 }
