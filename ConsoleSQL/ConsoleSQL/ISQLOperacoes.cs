44fb62297e74d0955d365a7b81907101041bae49
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-29 19:12:04 -0300

    retirada do modificador de acesso public

--- ConsoleSQL/ConsoleSQL/ISQLOperacoes.cs
+++ ConsoleSQL/ConsoleSQL/ISQLOperacoes.cs
@@ -5,5 +5,5 @@ public interface ISQLOperacoes
     string Insert(string nomeDaTabela, int Id);
     string Update(string nomeDaTabela, int Id);
     string Read(string nomeDaTabela, int Id);
-    public string Delete(string nomeDaTabela, int Id);
+    string Delete(string nomeDaTabela, int Id);
 }
