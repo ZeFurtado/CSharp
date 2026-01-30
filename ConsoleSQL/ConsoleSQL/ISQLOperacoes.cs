cd8dd8b341afa421340c85aba9db81bc8d4893e6
ZeFurtado <lukhasfurtado9@gmail.com>
2026-01-30 19:40:49 -0300

    Deletada interface ISQLOperacoes

--- ConsoleSQL/ConsoleSQL/ISQLOperacoes.cs
+++
@@ -1,9 +0,0 @@
-namespace ConsoleSQL;
-
-public interface ISQLOperacoes
-{
-    string Insert(string nomeDaTabela, int Id);
-    string Update(string nomeDaTabela, int Id);
-    string Read(string nomeDaTabela, int Id);
-    string Delete(string nomeDaTabela, int Id);
-}
