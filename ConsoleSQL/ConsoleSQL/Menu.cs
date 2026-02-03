be36c5a45d47814fa708160dc87a2017c630e228
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-02 21:42:26 -0300

    Adicionando as telas que serão exibidas para o usuário ao executar o programa

--- ConsoleSQL/ConsoleSQL/Menu.cs
+++ ConsoleSQL/ConsoleSQL/Menu.cs
@@ -1,4 +1,6 @@
+using System.Data.SqlTypes;
 using System.Dynamic;
+using Microsoft.Data.SqlClient;
 using Microsoft.Identity.Client;
 
 namespace ConsoleSQL;
@@ -28,6 +30,48 @@ ________________
 
         return titulo;
 
-    }   
+    }
+
+    public static void Login()
+    {
+        Console.WriteLine(Titulo("Login"));
+        Console.WriteLine("Digite a senha do banco de dados:");
+    }
+
+    public static void TelaInicial()
+    {
+        Console.WriteLine(Titulo("Tela Inicial"));
+        Console.WriteLine("Você está utilizando a base de dados: {0}");
+        Console.WriteLine("O que você deseja fazer com a tabela?");
+        Console.WriteLine("1 - Inserir");
+        Console.WriteLine("2 - Editar");
+        Console.WriteLine("3 - Consultar");
+        Console.WriteLine("4 - Deletar");
+        Console.WriteLine("5 - Sair"); 
+    }
+
+    public static void Insert(SqlConnection sqlConnection)
+    {
+        Console.WriteLine(Titulo("Insert"));
+                
+        Console.WriteLine("Digite o nome do usuário: ");
+        string nome = Console.ReadLine();
+                
+        Console.WriteLine("Digite o nome do cargo do usuário: ");
+        string cargo = Console.ReadLine();
+
+        Console.WriteLine("Digite seu melhor e-mail: ");
+        string email = Console.ReadLine();
+
+        Console.Write("\aInserindo dados no banco.");
+        Thread.Sleep(950);
+        Console.Write(".");
+        Thread.Sleep(950);
+        Console.WriteLine(".");
+        Thread.Sleep(850);
+        int linhasAfetadas = SQLOperacoes.Insert(/*sqlConnection*/nome, cargo, email);
+        Console.WriteLine("O comando alterou {0} linha/s", linhasAfetadas);
+        Thread.Sleep(1500);   
+    }
     
 }
