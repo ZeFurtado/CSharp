556bae0a878eb1060bfba1ac5244da12d6a08ac1
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-03 19:36:31 -0300

    While para o fluxo principal do programa e nova implementação da conexão do banco de dados funcionando.

--- ConsoleSQL/ConsoleSQL/Program.cs
+++ ConsoleSQL/ConsoleSQL/Program.cs
@@ -7,38 +7,29 @@ public class Program
     static void Main()
     {
         int opcao = 0;
+        SqlConnection sqlConnection;
         while(opcao != 5)
         {
             Console.Clear();
-            SqlConnection sqlConnection;
-            switch(opcao)
+            Menu.Login();
+            string senhaDB = Console.ReadLine();
+            if(!SQLOperacoes.CriarConexao(senhaDB, out sqlConnection))
             {
-                case 0:
-                    Menu.Login();
-                    string senhaDB = Console.ReadLine();
-                    if(!SQLOperacoes.CriarConexao(senhaDB, out sqlConnection))
-                    {
-                        Console.WriteLine("A conexão com o banco de dados falhou");
-                        Thread.Sleep(1500);
-                        Console.WriteLine("Pressione qualquer tecla para tentar novamente.");
-                        Console.ReadLine();
-                        opcao = 0;
-                    }else
-                    {
-                        Menu.TelaInicial();
-                        opcao = int.Parse(Console.ReadLine());
-                    }      
-                break;
-
-                case 1:
-                    Menu.Insert(sqlConnection);
-                break;
-
-                default:
-                    Console.WriteLine("Digite uma das opções acima.");
-                break;
+                Console.WriteLine("A conexão com o banco de dados falhou");
+                Thread.Sleep(1500);
+                Console.WriteLine("Pressione qualquer tecla para tentar novamente.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Conexão bem sucedida");
+                Thread.Sleep(1500);
+                Console.Clear();
+                Menu.TelaInicial();
+                opcao = int.Parse(Console.ReadLine());
             }
         }
+         
         
     }
 }
