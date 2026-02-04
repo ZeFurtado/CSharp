5fe59e8d6eda3959e7e929dff63a22bf032ac997
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-04 20:42:51 -0300

    Implementação de um laço for para a tela de login do programa e criado método estático Programa() para operar o fluxo de execução do programa

--- ConsoleSQL/ConsoleSQL/Program.cs
+++ ConsoleSQL/ConsoleSQL/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.IO.Compression;
+using Microsoft.Data.SqlClient;
 
 namespace ConsoleSQL;
 
@@ -6,10 +7,10 @@ public class Program
 {
     static void Main()
     {
-        int opcao = 0;
-        string sqlConnectionString;
-        while(opcao != 5)
+        for(int t = 1; t < 4; t++)
         {
+            int opcao = 0;
+            string sqlConnectionString;
             Console.Clear();
             Menu.Login();
             string senhaDB = Console.ReadLine();
@@ -17,42 +18,51 @@ public class Program
             {
                 Console.WriteLine("A conexão com o banco de dados falhou");
                 Thread.Sleep(1500);
-                Console.WriteLine("Pressione qualquer tecla para tentar novamente.");
-                Console.ReadLine();
-            }
-            else
-            {
-                Console.WriteLine("Conexão bem sucedida");
-                Thread.Sleep(1500);
-                Console.Clear();
-                Menu.TelaInicial();
-                opcao = int.Parse(Console.ReadLine());
 
-                switch(opcao)
+                if(t == 3)
                 {
-                    case 1:
-                        try
-                        {
-                            Menu.Insert(sqlConnectionString);
-                            Console.WriteLine(sqlConnectionString);  
-                        }
-                        catch(Exception ex)
-                        {
-                            Console.WriteLine("Ocorreu um erro ao tentar inserir no banco: {0}", ex.Message);
-                            Thread.Sleep(1000);
-                            Console.WriteLine("Pressione qualquer tecla para continuar.");
-                            Console.ReadLine();
-                        }
-                        break;
-
-                    default:
-                        opcao =  6;
-                        break;
-
-                }
+                    Console.WriteLine("Número de tentativas excedido!");
+                    break;   
+                } 
+                Console.WriteLine("Pressione qualquer tecla para tentar novamente. Tentativa {0} de 3", t);
+                Console.ReadLine();
+            }else
+            {
+               Programa(ref sqlConnectionString);
+               t = 4; 
             }
-        }
-         
+        }    
+    }
+
+    static void Programa(ref string sqlConnectionString)
+    {
+        int opcao = 0;
+        Console.WriteLine("Conexão bem sucedida");
+        Thread.Sleep(1500);
+        Console.Clear();
         
+        while(opcao < 5)
+        {
+            Menu.TelaInicial();
+            opcao = int.Parse(Console.ReadLine());
+            switch(opcao)
+            {
+                case 1: 
+                    Menu.Insert(sqlConnectionString);
+                    Console.WriteLine(sqlConnectionString);  
+                 break;
+
+                case 5:
+                    opcao = 5;
+                 break;
+
+                default:
+                    Console.WriteLine("Opção {0} não existe", opcao);
+                    Console.ReadLine();
+                    opcao = 0;
+                 break;
+            }
+            
+        }
     }
 }
