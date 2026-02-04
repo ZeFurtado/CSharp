6b1ca5db4d2d90f816500371759e1091a15aae9a
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-03 21:29:22 -0300

    Recebendo por paramêtro out a string de conexão

--- ConsoleSQL/ConsoleSQL/Program.cs
+++ ConsoleSQL/ConsoleSQL/Program.cs
@@ -7,13 +7,13 @@ public class Program
     static void Main()
     {
         int opcao = 0;
-        SqlConnection sqlConnection;
+        string sqlConnectionString;
         while(opcao != 5)
         {
             Console.Clear();
             Menu.Login();
             string senhaDB = Console.ReadLine();
-            if(!SQLOperacoes.CriarConexao(senhaDB, out sqlConnection))
+            if(!SQLOperacoes.CriarConexao(senhaDB, out sqlConnectionString))
             {
                 Console.WriteLine("A conexão com o banco de dados falhou");
                 Thread.Sleep(1500);
@@ -27,6 +27,29 @@ public class Program
                 Console.Clear();
                 Menu.TelaInicial();
                 opcao = int.Parse(Console.ReadLine());
+
+                switch(opcao)
+                {
+                    case 1:
+                        try
+                        {
+                            Menu.Insert(sqlConnectionString);
+                            Console.WriteLine(sqlConnectionString);  
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.WriteLine("Ocorreu um erro ao tentar inserir no banco: {0}", ex.Message);
+                            Thread.Sleep(1000);
+                            Console.WriteLine("Pressione qualquer tecla para continuar.");
+                            Console.ReadLine();
+                        }
+                        break;
+
+                    default:
+                        opcao =  6;
+                        break;
+
+                }
             }
         }
          
