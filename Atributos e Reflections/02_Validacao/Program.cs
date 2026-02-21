574d4dd515468bf7da45ff1997e5383beb0bf2fa
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-20 23:38:54 -0300

    Testando execução dos métodos da classe  Resx

--- Atributos e Reflections/02_Validacao/Program.cs
+++ Atributos e Reflections/02_Validacao/Program.cs
@@ -6,6 +6,17 @@ public class Program
 {
     static void Main(string[] args)
     {
+
+        Console.WriteLine(Resx.CheckFile());
+        Console.ReadLine();
+        
+        try
+        {
+            Resx.AddToFile(new Resx{Name="MSG_ERRO", Value="Erro", Comment="Mensagem de Erro"});    
+        }catch(Exception ex)
+        {
+            Console.WriteLine("Mensagem ERRO {0}", ex.Message );
+        }
         
     }
 }
