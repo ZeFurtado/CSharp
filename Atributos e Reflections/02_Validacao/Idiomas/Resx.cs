d80e1f2791e4c1aed41f287fb6f444f07cacf68d
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-21 01:11:13 -0300

    Método para deserializar o arquivo xml e retorna-lo em um objeto tipo Resx

--- Atributos e Reflections/02_Validacao/Idiomas/Resx.cs
+++ Atributos e Reflections/02_Validacao/Idiomas/Resx.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
 using System.Xml;
 using System.Xml.Serialization;
 using static System.Xml.XmlTextWriter;
@@ -31,4 +32,22 @@ public class Resx
         }
         
     }
+
+    
+    public static Resx Deserializacao()
+    {
+        try
+        {
+            using(StreamReader stream = new StreamReader("/home/lukhas/repo/CSharp/Atributos e Reflections/02_Validacao/Idiomas/Idiomas.xml"))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Resx));
+                return (Resx)xmlSerializer.Deserialize(stream);
+            }
+        }catch(Exception ex)
+        {
+            Console.WriteLine("|Resx.Deserializacao()| Mensagem ERRO: {0}", ex.Message);
+            return new Resx{Name="", Value="", Comment=""};
+        } 
+    }
+    
 }
