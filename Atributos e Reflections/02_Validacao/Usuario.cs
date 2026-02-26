8aa78c6a6535e0115d76a256601b5122ca801e31
ZeFurtado <lukhasfurtado9@gmail.com>
2026-02-25 23:35:17 -0300

    Comentando o atributo ErrorMessageResourceType para consertar sua validação

--- Atributos e Reflections/02_Validacao/Usuario.cs
+++ Atributos e Reflections/02_Validacao/Usuario.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using _02_Validacao.Idiomas;
 
 namespace _02_Validacao;
 public class Usuario
 {
     [Required(ErrorMessage ="O nome é obrigatório")]
     public string Nome{get; set;}
-    //[Required(ErrorMessageResourceType = )]
+    //[Required(ErrorMessageResourceType = typeof(Resx), ErrorMessageResourceName = "MSG_ERRO")]
     [EmailAddress]
     public string Email{get;set;}
     [Required, StringLength(10, MinimumLength = 6)]
