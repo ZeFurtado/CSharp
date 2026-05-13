859ba339d226bbcc040ce8302f27650f06907be3
ZeFurtado <lukhasfurtado9@gmail.com>
2026-05-13 11:05:32 -0300

    Program.cs

--- Algoritmos e Estruturas de Dados/ListaProject/Program.cs
+++ Algoritmos e Estruturas de Dados/ListaProject/Program.cs
@@ -5,14 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Lista<string> listaString = new Lista<string>("Armindo");
-
+        listaString.AddItem("Bolsonaro");
+        listaString.AddItem("Lula");
+        listaString.AddItem("Carmen Lucia");
+        listaString.AddItem("Alexandre de Moraes");
+        listaString.AddItem("Amarildo");
+        
         var number = new BaseGenerics<int>(75);
 
         var name = new BaseGenerics<string>("Osvaldo");
 
         Console.WriteLine("Número de tipo genérico {0}", number.GetItem());
         Console.WriteLine("Nome de tipo genérico {0}", name.GetItem());
-        Console.WriteLine("Teste Lista String {0}", listaString.GetItem(0));
+        
+        
+        foreach(string s in listaString.GetValues())
+        {
+            Console.WriteLine("Nome: " + s);
+        }
+
+        listaString.RemoveItem(2);
+
+        foreach(string s in listaString.GetValues())
+        {
+            Console.WriteLine("Nome: " + s);
+        }
 
     }
 }
