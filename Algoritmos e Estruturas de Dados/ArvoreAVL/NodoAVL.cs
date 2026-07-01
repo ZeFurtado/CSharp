cd56bd8e7c0bb17371d8bf45ba1eb29c2b19a359
ZeFurtado <lukhasfurtado9@gmail.com>
2026-07-01 17:21:09 -0300

    Classe NodoAVL

---
+++ Algoritmos e Estruturas de Dados/ArvoreAVL/NodoAVL.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ArvoreAVL;
+
+public class NodoAVL<T>
+{
+    public T Item{get;set;}
+    public int Chave{get;} //Outras classes só poderão consultar a chave
+    public NodoAVL<T>? NodoDireita{get;set;}
+    public NodoAVL<T>? NodoEsquerda{get;set;}
+
+    public NodoAVL(T item)
+    {
+        Item = item;
+        Chave = Chave + 1;
+        NodoDireita = null;
+        NodoEsquerda = null;
+    }
+
+
+    public int altura(NodoAVL<T> raiz)
+    {
+        if(raiz == null)
+        {
+            return 0;
+        }else
+        {
+            int alturaDireita = altura(raiz.NodoDireita);
+            int alturaEsquerda = altura(raiz.NodoEsquerda);
+
+            return Math.Max(alturaDireita, alturaEsquerda) + 1;
+        }
+         
+    }
+
+    public int fatorBalanceamento()
+    {
+        return altura(NodoEsquerda) - altura(NodoDireita);
+    }
+
+    
+}
