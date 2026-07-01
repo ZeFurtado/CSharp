906e30058587fa12faa3ce4ad6fc6ae13d48c40d
ZeFurtado <lukhasfurtado9@gmail.com>
2026-07-01 19:40:07 -0300

    classe ArvoreAVL.cs

---
+++ Algoritmos e Estruturas de Dados/ArvoreAVL/ArvoreAVL.cs
@@ -0,0 +1,27 @@
+﻿namespace ArvoreAVL;
+
+public class ArvoreAVL<T>
+{
+    private NodoAVL<T> NodoRaiz;
+
+    public ArvoreAVL()
+    {
+        
+    }
+
+    public void Inserir(NodoAVL<T> item)
+    {
+        InserirRecursivo(item);
+    }
+
+    private void InserirRecursivo(NodoAVL<T> item)
+    {
+        if (NodoRaiz == null)
+        {
+            NodoRaiz = item;
+        }else
+        {
+            item.NodoDireita = item;
+        }
+    }
+}
