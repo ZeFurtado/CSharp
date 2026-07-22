be90b890f57f5052faa9f37f47cab11581ca2baa
ZeFurtado <lukhasfurtado9@gmail.com>
2026-07-22 19:28:58 -0300

    Chamadas de inserção, remoção para testar a árvore

--- Algoritmos e Estruturas de Dados/ArvoreAVL/Program.cs
+++ Algoritmos e Estruturas de Dados/ArvoreAVL/Program.cs
@@ -26,5 +26,36 @@ public class Program
 
         arvoreAVL.Inserir("Joniver");
         arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Cléber");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Hugo");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Remover(3);
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Thiago");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Juliano");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Júlio");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Júnior");
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Inserir("Alvares");
+        arvoreAVL.Inserir("Ricardinho");
+        arvoreAVL.Inserir("Soares");
+
+        arvoreAVL.PrintTree();
+
+        arvoreAVL.Remover(8);
+        arvoreAVL.PrintTree();
+        Console.WriteLine(arvoreAVL.ToString());
     }
 }
